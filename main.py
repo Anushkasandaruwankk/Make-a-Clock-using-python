3e4ed7495bd8f7bf25ba533d9ce949ef30bd27db
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-07-22 01:23:46 +0530

    co

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def time():
     label.config(text=string)
     label.after(1000, time)
 
-# label = Label(root, font=("ds-digital", 80), background = "black", foreground = "cyan")
+label = Label(root, font=("ds-digital", 80), background = "black", foreground = "cyan")
 # label.pack(anchor = 'center')
 # time()
 
