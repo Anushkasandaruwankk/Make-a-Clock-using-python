cd20b1b18ace07c37ae4cf0b5c465d5c49341a86
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-07-22 01:23:55 +0530

    co

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def time():
     label.after(1000, time)
 
 label = Label(root, font=("ds-digital", 80), background = "black", foreground = "cyan")
-# label.pack(anchor = 'center')
+label.pack(anchor = 'center')
 # time()
 
 # mainloop()
