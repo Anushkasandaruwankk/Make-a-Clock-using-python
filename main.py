734c7f6bee0fa2396a075bbaca1da08189fce37d
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-07-22 01:23:13 +0530

    co

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ root.title("Clock")
 
 def time():
     string = strftime('%H:%M:%S %p')
-#     label.config(text=string)
+    label.config(text=string)
 #     label.after(1000, time)
 
 # label = Label(root, font=("ds-digital", 80), background = "black", foreground = "cyan")
